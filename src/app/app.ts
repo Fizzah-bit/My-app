39b3b840f7665ac0460a8f5eceb8b7cf87198ed0
Fizzah <Fizzahirshad540@gmail.com>
2025-08-26 17:07:43 +0500

    First commit - Angular app setup

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,43 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']   
 })
-export class App {
+export class App{
   protected readonly title = signal('my-app1');
+  // Function sahi tarike se likhna
+  // handleClickEvent(): void {
+  //   alert("Function called");
+  // }
+  display=true
+  x=20
+  color='blue'
+   fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"];
+  HandleColor(val:string){
+    this.color=val
+  }
+
+ // Signal banana
+  counter = signal(0);
+
+  // Signal update karne ke liye methods
+  increment() {
+    this.counter.update(value => value + 1);
+  }
+
+  decrement() {
+    this.counter.update(value => value - 1);
+  }
+
+  reset() {
+    this.counter.set(0);
+  }
+
+
+
+  
 }
