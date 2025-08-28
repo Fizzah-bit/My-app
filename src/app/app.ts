da7a175fecdbab2d72a01557e50e9d41beaee76c
Fizzah <Fizzahirshad540@gmail.com>
2025-08-28 17:56:12 +0500

    Your message about the changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,27 +1,28 @@
 import { Component, signal } from '@angular/core';
-
+import { CommonModule } from '@angular/common';   // 👈 for @if, @for, etc
+import { FormsModule } from '@angular/forms';     // 👈 for [(ngModel)]
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  standalone: true,   // 👈 standalone confirm
+  imports: [CommonModule, FormsModule],  // 👈 Required imports
   templateUrl: './app.html',
-  styleUrls: ['./app.css']   
+  styleUrls: ['./app.css']
 })
-export class App{
+export class App {
   protected readonly title = signal('my-app1');
-  // Function sahi tarike se likhna
-  // handleClickEvent(): void {
-  //   alert("Function called");
-  // }
-  display=true
-  x=20
-  color='blue'
-   fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"];
-  HandleColor(val:string){
-    this.color=val
+
+  display = true;
+  x = 20;
+  color = 'blue';
+
+  fruits = ['Apple', 'Banana', 'Mango', 'Orange', 'Grapes'];
+
+  HandleColor(val: string) {
+    this.color = val;
   }
 
- // Signal banana
+  // Signal banana
   counter = signal(0);
 
   // Signal update karne ke liye methods
@@ -37,7 +38,30 @@ export class App{
     this.counter.set(0);
   }
 
+  // Two way binding ke liye properties
+  username: string = '';
 
 
-  
+
+
+
+  task="";
+tasklist:{id:number,task:string}[]=[]
+ addtask(){
+  this.tasklist.push({id:this.tasklist.length+1,task:this.task})
+  console.log(this.tasklist)
+ }
+
+Deletetask(taskid:number){
+  this.tasklist=this.tasklist.filter((item)=>taskid !=taskid);
+
+}
+
+isActive = false;
+ boxColor: string = 'lightgray';
+
+  // Method to change color on button click
+  changeColor(color: string) {
+    this.boxColor = color;
+  }
 }
